82503ae798f39c40ceac3523461ed90a7f46040c
Stefan Meyer <stefan-meyer@sinnerschrader.com>
2012-07-17 10:31:36 +0200

    fixed compile error

--- src/main/java/org/atemsource/atem/impl/json/JsonSchemaTransformation.java
+++ src/main/java/org/atemsource/atem/impl/json/JsonSchemaTransformation.java
@@ -2,16 +2,13 @@ package org.atemsource.atem.impl.json;
 
 import javax.annotation.Resource;
 
-import org.atemsource.atem.api.type.EntityTypeBuilder;
-import org.atemsource.atem.impl.json.schema.JsonSchema;
 import org.atemsource.atem.spi.DynamicEntityTypeSubrepository;
-import org.atemsource.atem.utility.transform.api.AttributeTransformationBuilder;
 import org.atemsource.atem.utility.transform.api.Transformation;
 import org.atemsource.atem.utility.transform.api.TransformationBuilderFactory;
-import org.atemsource.atem.utility.transform.api.TypeTransformationBuilder;
-import org.atemsource.atem.utility.transform.impl.builder.CollectionAttributeTransformationBuilder;
 import org.codehaus.jackson.node.ObjectNode;
 
+
+// import org.atemsource.atem.impl.json.schema.JsonSchema;
 
 public class JsonSchemaTransformation
 {
@@ -22,20 +19,21 @@ public class JsonSchemaTransformation
 	@Resource
 	private TransformationBuilderFactory transformationBuilderFactory;
 
-	public Transformation<JsonSchema, ObjectNode> createTransformation()
+	public Transformation<Object, ObjectNode> createTransformation()
 	{
-		EntityTypeBuilder jsonSchemaBuilder = jsonRepository.createBuilder("json:" + JsonSchema.class.getName());
-		TypeTransformationBuilder<JsonSchema, ?> builder =
-			transformationBuilderFactory.create(JsonSchema.class, jsonSchemaBuilder);
-		builder.transformPrimitives();
-		// builder.putDefaultConverter(new JsonSchemaUriConverter());
-		AttributeTransformationBuilder<JsonSchema, ?> superType = builder.transform();
-		superType.fromMethod().getSuperType();
-		superType.to("extends");
-		CollectionAttributeTransformationBuilder<JsonSchema, ?> properties = builder.transformCollection();
-		properties.fromMethod().getProperties();
-		// properties.convert(converter);
+		// EntityTypeBuilder jsonSchemaBuilder = jsonRepository.createBuilder("json:" + JsonSchema.class.getName());
+		// TypeTransformationBuilder<JsonSchema, ?> builder =
+		// transformationBuilderFactory.create(JsonSchema.class, jsonSchemaBuilder);
+		// builder.transformPrimitives();
+		// // builder.putDefaultConverter(new JsonSchemaUriConverter());
+		// AttributeTransformationBuilder<JsonSchema, ?> superType = builder.transform();
+		// superType.fromMethod().getSuperType();
+		// superType.to("extends");
+		// CollectionAttributeTransformationBuilder<JsonSchema, ?> properties = builder.transformCollection();
+		// properties.fromMethod().getProperties();
+		// // properties.convert(converter);
+		// return null;
+		//
 		return null;
-
 	}
 }

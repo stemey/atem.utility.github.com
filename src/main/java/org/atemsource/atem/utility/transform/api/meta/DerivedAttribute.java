c77d11985c83cb7ad913f4e2bdfcf6ccd5cf640c
Stefan Meyer <stefan-meyer@sinnerschrader.com>
2012-10-12 08:35:01 +0200

    removed @Override on method impl

--- src/main/java/org/atemsource/atem/utility/transform/api/meta/DerivedAttribute.java
+++ src/main/java/org/atemsource/atem/utility/transform/api/meta/DerivedAttribute.java
@@ -1,32 +1,29 @@
 /*******************************************************************************
- * Stefan Meyer, 2012
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Stefan Meyer, 2012 Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  ******************************************************************************/
 package org.atemsource.atem.utility.transform.api.meta;
 
-import org.atemsource.atem.utility.transform.api.AttributeTransformation;
-
 import org.atemsource.atem.api.attribute.Attribute;
 import org.atemsource.atem.impl.meta.DerivedObject;
+import org.atemsource.atem.utility.transform.api.AttributeTransformation;
 
 
 public class DerivedAttribute implements DerivedObject
 {
 
+	private Attribute<?, ?> originalAttribute;
+
 	private AttributeTransformation<?, ?> transformation;
 
-	private Attribute<?, ?> originalAttribute;
+	@Override
+	public Object getOriginal()
+	{
+		return originalAttribute;
+	}
 
 	public Attribute<?, ?> getOriginalAttribute()
 	{
@@ -46,11 +43,6 @@ public class DerivedAttribute implements DerivedObject
 	public void setTransformation(AttributeTransformation<?, ?> transformation)
 	{
 		this.transformation = transformation;
-	}
-
-	@Override
-	public Object getOriginal() {
-		return originalAttribute;
 	}
 
 }

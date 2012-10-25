fd466eec7a4e9081aff99514ecfab35dccc29431
Stefan Meyer <stefan.meyer@sinnerschrader.com>
2012-10-25 07:25:26 +0200

    refactored configuration

---
+++ src/main/java/org/atemsource/atem/utility/transform/impl/converter/DateConverter.java
@@ -0,0 +1,62 @@
+package org.atemsource.atem.utility.transform.impl.converter;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.atemsource.atem.api.infrastructure.exception.ConversionException;
+import org.atemsource.atem.utility.transform.api.JavaConverter;
+import org.atemsource.atem.utility.transform.api.TransformationContext;
+import org.atemsource.atem.utility.transform.api.constraint.DateFormat;
+
+public class DateConverter implements JavaConverter<Date, String>,Constraining {
+	private DateFormat dateFormat;
+	private SimpleDateFormat simpleDateFormat;
+
+	public void setPattern(String pattern) {
+		simpleDateFormat = new SimpleDateFormat(pattern);
+		this.dateFormat = new DateFormat(pattern, simpleDateFormat);
+	}
+
+	@Override
+	public String convertAB(Date a, TransformationContext ctx) {
+		if (a == null) {
+			return null;
+		} else {
+			return simpleDateFormat.format(a);
+		}
+	}
+
+	@Override
+	public Date convertBA(String b, TransformationContext ctx) {
+		if (b == null) {
+			return null;
+		} else {
+			try {
+				return simpleDateFormat.parse(b);
+			} catch (ParseException e) {
+				throw new ConversionException(b,Date.class);
+			}
+		}
+	}
+
+	@Override
+	public String[] getConstraintNamesAB() {
+		return new String[]{DateFormat.META_ATTRIBUTE_CODE};
+	}
+
+	@Override
+	public Object getConstraintAB(String name) {
+		return dateFormat;
+	}
+
+	@Override
+	public String[] getConstraintNamesBA() {
+		return new String[0];
+	}
+
+	@Override
+	public Object getConstraintBA(String name) {
+		return null;
+	}
+}

a4354a877e259c284a26df81110c03011e14b235
Stefan Meyer <stefan-meyer@sinnerschrader.com>
2012-12-05 15:50:35 +0100

    added logging

--- src/main/java/org/atemsource/atem/utility/transform/impl/transformation/OneToOneAttributeTransformation.java
+++ src/main/java/org/atemsource/atem/utility/transform/impl/transformation/OneToOneAttributeTransformation.java
@@ -3,58 +3,81 @@ package org.atemsource.atem.utility.transform.impl.transformation;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.atemsource.atem.api.infrastructure.exception.TechnicalException;
 import org.atemsource.atem.utility.path.AttributePath;
-import org.atemsource.atem.utility.transform.api.Converter;
 import org.atemsource.atem.utility.transform.api.Transformation;
 import org.atemsource.atem.utility.transform.api.TransformationContext;
-import org.atemsource.atem.utility.transform.api.UniConverter;
 import org.atemsource.atem.utility.transform.api.UniTransformation;
 
-public abstract  class OneToOneAttributeTransformation<A, B>  extends AbstractAttributeTransformation<A, B>{
+
+public abstract class OneToOneAttributeTransformation<A, B> extends AbstractAttributeTransformation<A, B>
+{
 
 	private Transformation transformation;
 
-	public void mergeBA(B b, A a, TransformationContext ctx) {
-		UniTransformation baConverter = transformation == null ? null : transformation.getBA();
-		transformInternally(b, a,
-				getAttributeB(), getAttributeA(), ctx, baConverter);
-	}
-
-	protected AttributePath getAttributeA() {
+	protected AttributePath getAttributeA()
+	{
 		return getAttributeAs().iterator().next();
 	}
 
-	protected AttributePath getAttributeB() {
+	protected AttributePath getAttributeB()
+	{
 		return getAttributeBs().iterator().next();
 	}
 
-	public void mergeAB(A a, B b, TransformationContext ctx) {
-		UniTransformation abConverter = transformation == null ? null : transformation.getAB();
-		transformInternally(a, b,
-				getAttributeA(), getAttributeB(), ctx, abConverter);
-	}
-
-	public Transformation getTransformation() {
+	public Transformation getTransformation()
+	{
 		return transformation;
 	}
 
-	public void setAttributeA(AttributePath source) {
+	@Override
+	public void mergeAB(A a, B b, TransformationContext ctx)
+	{
+		try
+		{
+			UniTransformation abConverter = transformation == null ? null : transformation.getAB();
+			transformInternally(a, b, getAttributeA(), getAttributeB(), ctx, abConverter);
+		}
+		catch (Exception e)
+		{
+			throw new TechnicalException("cannot transform " + getAttributeA().getAsString() + " with value " + a, e);
+		}
+	}
+
+	@Override
+	public void mergeBA(B b, A a, TransformationContext ctx)
+	{
+		UniTransformation baConverter = transformation == null ? null : transformation.getBA();
+		try
+		{
+			transformInternally(b, a, getAttributeB(), getAttributeA(), ctx, baConverter);
+		}
+		catch (Exception e)
+		{
+			throw new TechnicalException("cannot transform " + getAttributeB().getAsString() + " with value " + b, e);
+		}
+	}
+
+	public void setAttributeA(AttributePath source)
+	{
 		Set<AttributePath> attributeAs = new HashSet<AttributePath>();
 		attributeAs.add(source);
 		setAttributeAs(attributeAs);
 	}
 
-	public void setAttributeB(AttributePath attributeB) {
+	public void setAttributeB(AttributePath attributeB)
+	{
 		Set<AttributePath> attributeBs = new HashSet<AttributePath>();
 		attributeBs.add(attributeB);
 		setAttributeBs(attributeBs);
 	}
 
-	public void setTransformation(Transformation transformation) {
+	public void setTransformation(Transformation transformation)
+	{
 		this.transformation = transformation;
 	}
 
-	protected abstract void transformInternally(Object a, Object b,
-			AttributePath attributeA, AttributePath attributeB, TransformationContext ctx, UniTransformation<Object, Object> ab);
+	protected abstract void transformInternally(Object a, Object b, AttributePath attributeA, AttributePath attributeB,
+		TransformationContext ctx, UniTransformation<Object, Object> ab);
 
 }
